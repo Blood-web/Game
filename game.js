3b03101d4e8e4400a1a8c837b8c28b4eeff3fb8c
Blood-Web <jack.c.ewers@outlook.com>
2022-01-26 11:08:54 +0800

    push for george
    menu key
    encrypt, local+
    null void
    
    data null
    
    Co-Authored-By: BloodWeX <98154687+BloodWeX@users.noreply.github.com>

--- game.js
+++ game.js
@@ -5,6 +5,19 @@ idjcounter = 0;
 idj = idjcounter;
 
 menukey="//check//";
+function createmenukey(){
+    Mkey=fireupgrade.level+" "+arcaneupgrade.level+" "+"PH-ice"+" "+earthupg.level+" "+menu.allspellsupglevel+" "+autoupg.level+
+    ' \n'+ menu.m10X+" "+menu.mmlevel+" "+ "PH";
+    breakmenukey();
+}
+function breakmenukey(){
+     for (var i = 0; i < Mkey.length; i++) {   
+        if (Mkey[i] == undefined || Mkey[i] == null) {
+            Mkey[i] = 0; }   }
+    fireupgrade.level=Mkey[0]; arcaneupgrade.level=Mkey[1]; "PHICE";
+    earthupg.level=Mkey[3]; menu.allspellsupglevel=Mkey[4]; autoupg.level=Mkey[5]; 
+    console.log(Mkey +" Length"+Mkey.length);
+}
 var playertoken="";
 const space=' ';
 function createtoken(){
@@ -31,7 +44,7 @@ function breakplayertoken(token, seperator){
 ;
     console.log('original string:'+token);
     console.log("The array has "+tokensplit.length+" elements: "+ tokensplit.join(' / '));
-    message.innerText=tokensplit[2];
+message.innerText=tokensplit[2];
 }
 let score = 0;
 let number=0;
@@ -53,12 +66,17 @@ const menu = {
     prestigetab:document.getElementById("prestigetab"),
     prestiget:document.getElementById("prestigetable"),
     prestigetext:document.getElementById("prestigetext"),
-    //ROW1//
-    fireballxupg$:0,    allspellsupg$:0,    
-    allspellsupglevel:0,
+   
+    //ROW1 -- spells can be found in relative + UPG//
+
+    allspellsupg$:0,    
+    allspellsupglevel:2, achelplevel:0,
+    
     //row2//
+    m10X:0,// boo 1-0//
     manaregen10upg$:42000,
     manaregenupgdesc:document.getElementById('pr2c1'),
+    mmlevel:0,
     manamaxupg$:25000,
     manamaxupgdesc:document.getElementById('pr2c2'),
     earthquakeupg$:100000,
@@ -145,8 +163,7 @@ const auto ={
     truestatus:"",}
 const autoupg={
     level:0,
-    upg$:600000,
-    
+    upg$:600000,    
 }
 const mana = {
     level:0,
@@ -575,7 +592,7 @@ else  if (click > auto.upg$ && auto.level!=0){
     updateclick();
     updateautoclicker();  
     levelup(); 
-}}
+    }}
 
 function setstatusauto(){
     updateautoclicker();
@@ -590,11 +607,9 @@ function setstatusauto(){
     }
     else if (auto.status=="basicbitch"){
         auto.master.style.opacity=0;
-    }
-}
+    } }
 
 function updateautoclicker(){
-
     number = auto.upg$; gennumberconverter();  auto.cc.innerText=number;
     
     if (click < auto.upg$){
@@ -797,26 +812,21 @@ function setstatusfire(){
     }  
     else if(fire.image.classList.contains("firecooldown") || mana.player <= fire.castcost){
         fire.image.style.opacity=0.9;
-        //absolutefalse//
+        //absolutefalse//   
     } 
     else {
-        console.log("failed to set fireball status");
-    }
-}
+        console.log("failed to set fireball status"); } }
 
 function updatefirecounter(){
     fire.truestatus="level: "+fire.level+" "+fire.cast+" val: "+fire.val+
     " cast#: "+fire.castcounter+" upg$: "+fire.upgradecost+" cast$: "+fire.castcost+
-    " upg: "+fire.upgrade + " class: "+ fire.image.classList+" status: "+fire.status+'\n'+" src: "+fire.image.src
-   ;
+    " upg: "+fire.upgrade + " class: "+ fire.image.classList+" status: "+fire.status+'\n'+" src: " +fire.image.src ;
    
     fire.counter.innerText=fire.castcounter;
     fire.levelcounter.innerText=fire.level;
     number=fire.upgradecost; gennumberconverter(); fire.p4.innerText=number;
-    fire.cc.innerText=fire.castcost; 
-    }
+    fire.cc.innerText=fire.castcost; }
     
-
 function fireballupgrade(){
     firecostcheck();
     if (fire.btn.style.opacity >=1 && click >= fire.upgradecost){
@@ -1204,7 +1214,8 @@ function startrage(){
         //P inc +1, Multiplier x Plevel //
         function Prestige(){
              if (prestige.prog >= prestige.req && prestige.boo==true) {
-                 //..Level UP prestige//
+                //..Level UP prestige//
+
                 prestige.prog=0; prestige.level+=1; 
                 levelup();  updateprestigecounter();
                 console.log( clicker.multi + ' level = ' + prestige.level + '. user has prestiged');
@@ -1214,8 +1225,7 @@ function startrage(){
              }    }
              function updateprestigecounter(){
             if (prestige.level==null){prestige.level="0";  }
-                
-
+        
             prestige.req=prestige.level+1000*1.57 ; prestige.req = Math.floor(prestige.req); 
             prestige.levelcount.innerText = prestige.level;   prestige.progcount.innerText=prestige.prog;
             prestige.bar.value=prestige.prog;   prestige.bar.max=prestige.req;
